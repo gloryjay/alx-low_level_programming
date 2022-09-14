4dc5bd5ab20446a4d7c031a0993f17b1f00709d5
gloryjay <gloryjackson1801@gmail.com>
2022-09-14 16:30:17 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,21 @@
 #include "main.h"
 /**
- * main - entry point
+ * main - prints Putchar
+ *
+ * Description: prints _putchar
+ *
  * Return: 0 (Success)
  */
 int main(void)
 {
-int i = 0;
-char text[] = "_putchar";
-
-while (text[i] != '\0')
-{
-_putchar(text[i]);
-i++;
-}
+putchar(');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 
 return (0);
