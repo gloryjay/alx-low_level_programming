b342d40fbeb356df0c46be3ef1dcb0ce42a36740
gloryjay <gloryjackson1801@gmail.com>
2022-09-18 15:34:21 -0700

    alx functions nested task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function that prints abc 10 times
+ *
+ * Return: 0
+ */
+void print_alphabet_10x(void)
+{
+	char c, i:
+	{
+		for (i = 0; i <= 9;  i++)
+		{
+			_putchar(c);
+		}
+	_putchar('\n');
+	}
+}
