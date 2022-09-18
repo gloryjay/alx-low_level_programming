f31de257cfff0c1ef4761adb0cb8648220669f12
gloryjay <gloryjackson1801@gmail.com>
2022-09-18 15:42:47 -0700

    alx functins nested task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,14 +4,15 @@
  *
  * Return: 0
  */
-void print_alphabet_10x(void)
+void print_alphabet_x10(void)
 {
-	char c, i:
-	{
+	char c, i;
 		for (i = 0; i <= 9;  i++)
 		{
-			_putchar(c);
+			for (c = 'a'; c <= 'z'; c++)
+			{
+				_putchar(c);
+			}
+			_putchar('\n');
 		}
-	_putchar('\n');
-	}
 }
