7920594237f2b48abc7276a0fe2b940f0dc5d4df
gloryjay <gloryjackson1801@gmail.com>
2022-09-19 12:57:06 -0700

    C pointers arrays task 2 and 3

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,6 +4,11 @@
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 
 #endif /* MAIN_H*/
